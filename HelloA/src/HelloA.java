31bd15235e331dfd166986e1d539632ea484417c
denglun0302 <2393751439@qq.com>
2019-11-19 13:01:25 +0800

    提交代码

---
+++ HelloA/src/HelloA.java
@@ -0,0 +1,35 @@
+
+public class HelloA {
+	
+		//构造⽅法
+		public HelloA(){
+		System.out.println("Hello A!⽗类构造⽅法");
+		}
+		//⾮静态代码块
+		{
+		System.out.println("i'm A class.⽗类⾮静态代码块");
+		}
+		//静态代码块
+		static{
+		System.out.println("static A ⽗类静态代码块");
+		} }
+       class HelloB extends HelloA {
+	//构造⽅法
+	public HelloB(){
+	System.out.println("Hello B! 构造⽅法");
+	}
+	//⾮静态代码块
+	{
+	System.out.println("i'm B class.⾮静态代码块");
+	}
+	//静态代码块
+	static{
+	System.out.println("static B 静态代码块");
+	}
+	public static void main(String[] args) {
+	System.out.println("---start---");
+	new HelloB();
+	new HelloB();
+	System.out.println("---end---");
+	} }
+
